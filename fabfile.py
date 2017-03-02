88c3fba1703a52c103482f8e6e108a6c58c51429
dengshilong <dengshilong1988@gmail.com>
2017-03-02 10:53:50 +0800

    fix: fix fabfile.py

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,11 @@
 from fabric.api import *
-env.hosts = ['43.242.128.158']
-env.user = 'dengsl'
+env.use_ssh_config = True
+env.hosts = ['dsl']
 def deploy():
-    code_dir = '/home/dengsl/program/nodejs/blog'
+    code_dir = '~/program/nodejs/blog'
     with cd(code_dir):
         run("git pull")
          #deploy static site
         run("hexo clean")
         run("hexo g")
-        run("cp -r public/* /home/dengsl/program/html/blog/note")
+        run("cp -r public/* ~/program/html/blog/note")

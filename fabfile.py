4fe6cd99fb768b69130f06de2e7964011f8ec3b8
dengshilong <dengshilong1988@gmail.com>
2024-03-24 21:35:47 +0800

    docs: 增加Node服务增加强制内存回收功能

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,17 @@
 from fabric.api import *
-env.use_ssh_config = True
-env.hosts = ['dsl']
+# env.use_ssh_config = True
+# env.hosts = ['dsl']
+# def deploy():
+#     code_dir = '~/program/nodejs/program.dengshilong.org'
+#     with cd(code_dir):
+#         run("git pull")
+#          #deploy static site
+#         run("hexo clean")
+#         run("hexo g")
+#         run("cp -r public/* ~/program/html/blog/note")
+
+# from fabric import Connection
+
 def deploy():
     code_dir = '~/program/nodejs/program.dengshilong.org'
     with cd(code_dir):
